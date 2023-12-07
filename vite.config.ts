f698191aff5cb598beba06375eef63edd9663ec2
Ammiel Yawson <ammielgyanyawson@gmail.com>
2023-12-07 15:05:04 +0000

    chore: lint vite config ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import eslintPlugin from "vite-plugin-eslint";
-import svgrPlugin from "vite-plugin-svgr";
-import { defineConfig } from "vitest/config";
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import eslintPlugin from 'vite-plugin-eslint';
+import svgrPlugin from 'vite-plugin-svgr';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,40 +10,40 @@ export default defineConfig({
     react(),
     eslintPlugin({
       cache: false,
-      include: ["./src/**/*.ts", "./src/**/*.tsx"],
+      include: ['./src/**/*.ts', './src/**/*.tsx'],
       exclude: [],
     }),
     svgrPlugin({
       svgrOptions: {
         icon: true,
       },
-      include: "./src/assets/icons/*.svg",
+      include: './src/assets/icons/*.svg',
     }),
   ],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    outDir: "dist/app",
+    outDir: 'dist/app',
   },
   server: {
     port: 5200,
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTest.ts",
+    environment: 'jsdom',
+    setupFiles: './src/setupTest.ts',
     // testMatch: "./src/**/__tests__/*.{test,spec}.ts",
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: false,
     coverage: {
-      reporter: ["html", "clover", "json", "lcov"],
-      exclude: ["**/src/config/**"],
-      provider: 'v8'
+      reporter: ['html', 'clover', 'json', 'lcov'],
+      exclude: ['**/src/config/**'],
+      provider: 'v8',
     },
   },
 });
