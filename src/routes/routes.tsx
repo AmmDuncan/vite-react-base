41ee475637a6d0bf9da372c8bfcab34b75c402f3
Ammiel Yawson <ammielgyanyawson@gmail.com>
2023-12-10 17:59:37 +0000

    patch: remove icon from default route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,5 @@
 import { type RouteObject } from 'react-router-dom';
 
-import { Icon } from '@/components';
 import { UserList } from '@/components/UserList';
 
 export const routes: RouteObject[] = [
@@ -9,7 +8,6 @@ export const routes: RouteObject[] = [
     element: (
       <div className="h-screen text-center">
         <h1 className="py-6">Carduvy Frontend</h1>
-        <Icon name="Eyedafd" />
         <UserList />
       </div>
     ),
